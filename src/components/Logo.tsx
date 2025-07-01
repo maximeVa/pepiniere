277f47d048f0e3d13bcc96ac583b1a977a0614b1
maximeVa <maxime-v@live.be>
2025-07-01 17:55:01 +0200

    better logo border inside navbar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,7 +11,7 @@ export default function Logo({ className = '', scrolled = false }: { className?:
           width={120}
           height={45}
           sizes="(max-width: 640px) 80px, (max-width: 768px) 100px, 120px"
-          className="w-20 h-auto sm:w-24 md:w-28 lg:w-32 xl:w-36 border-2 border-white/80 rounded-lg transition-all duration-300 group-hover:border-white group-hover:scale-105"
+          className={`w-20 h-auto sm:w-24 md:w-28 lg:w-32 xl:w-36 border rounded-lg transition-all duration-300 group-hover:scale-105 ${scrolled ? 'border-gray-400/60 group-hover:border-gray-600/60' : 'border-gray-100/60 group-hover:border-gray-300/60'}`}
           priority
         />
       </Link>
